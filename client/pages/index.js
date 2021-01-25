569b9b5a10dbbb1c620810ab8378c6609218a2a8
nurulakbaral <nurulakbaral@gmail.com>
2021-01-25 16:03:07 +0700

    client init

---
+++ client/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
+  )
+}
+
+export default Home
